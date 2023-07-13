ad97f8c66b50f193e2af50542cc85f13deb1ec1a
sschum <s.schumann@tarent.de>
2023-07-13 15:02:53 +0200

    update

---
+++ src/js/card.js
@@ -0,0 +1,11 @@
+MyVueApp.component('card', {
+    template: `
+<div class="card">
+    <div>
+        <div class="card-body">
+            <h5 class="card-title"></h5>
+        </div>
+    </div>
+</div>
+    `
+})
